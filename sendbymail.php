628980da3e24ba78dd6337693755fcc30a0e5a08
Aldo Martinez Amador <mtz.aldo24@gmail.com>
2017-05-22 21:12:03 -0500

    Cambio Form

--- sendbymail.php
+++ sendbymail.php
@@ -10,7 +10,7 @@ $email_subject = "Contacto desde el sitio web";
 if(!isset($_POST['first_name']) ||
 // !isset($_POST['last_name']) ||
 !isset($_POST['email']) ||
-!isset($_POST['telephone']) ||
+// !isset($_POST['telephone']) ||
 !isset($_POST['comments'])) {
 
 echo "<b>Ocurrió un error y el formulario no ha sido enviado. </b><br />";
@@ -22,7 +22,7 @@ $email_message = "Detalles del formulario de contacto:\n\n";
 $email_message .= "Nombre: " . $_POST['first_name'] . "\n";
 // $email_message .= "Apellido: " . $_POST['last_name'] . "\n";
 $email_message .= "E-mail: " . $_POST['email'] . "\n";
-$email_message .= "Teléfono: " . $_POST['telephone'] . "\n";
+// $email_message .= "Teléfono: " . $_POST['telephone'] . "\n";
 $email_message .= "Comentarios: " . $_POST['comments'] . "\n\n";
 
 
@@ -30,7 +30,8 @@ $email_message .= "Comentarios: " . $_POST['comments'] . "\n\n";
 $headers = 'From: '.$email_from."\r\n".
 'Reply-To: '.$email_from."\r\n" .
 'X-Mailer: PHP/' . phpversion();
-@mail($email_to, $email_subject, $email_message, $headers);
+// @mail($email_to, $email_subject, $email_message, $headers);
+@mail ($email_to, $email_subject, $email_message, $headers);
 
 echo "¡El formulario se ha enviado con éxito!";
 }
